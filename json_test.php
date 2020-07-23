b5cc69a9bf71e3f912f9bd4e6b41aee804676a7c
veerasabari <vesabaresh@gmail.com>
2020-07-23 12:15:46 +0530

    Thurday update

---
+++ json_test.php
@@ -0,0 +1,18 @@
+<?php
+
+function isValidEmail($str){
+return filter_var($str, FILTER_VALIDATE_EMAIL);
+}
+
+
+//$emails = isset($_POST["emails"]) && is_array($_POST["emails"]) ? $_POST["emails"] : [];
+$emails = $_POST["emails"];
+
+$valid_emails = array_filter($emails, "isValidEmail");
+
+print_r($valid_emails);exit;
+
+header("Content-Type: application/json");
+echo json_encode($valid_emails);
+
+?>
